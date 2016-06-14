5411d5f277f65ba5fc2742a28f18ea7e783d4774
pengmengyang <646410289@qq.com>
2016-06-14 13:27:13 +0800

    1.1

--- Hello/src/com/example/hello/OtherActiviyt.java
+++ Hello/src/com/example/hello/OtherActiviyt.java
@@ -1,5 +1,14 @@
 package com.example.hello;
 
-public class OtherActiviyt {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class OtherActiviyt extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
 }
