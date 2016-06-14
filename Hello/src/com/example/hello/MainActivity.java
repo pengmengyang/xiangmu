1ff14413618e94817d63c0bbaa722b1cfc6ac09c
pengmengyang <646410289@qq.com>
2016-06-14 13:07:51 +0800

    首次提交到本地仓库

--- Hello/src/com/example/hello/MainActivity.java
+++ Hello/src/com/example/hello/MainActivity.java
@@ -10,5 +10,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        init();
     }
+
+	private void init() {
+		// TODO Auto-generated method stub
+		
+	}
 }
